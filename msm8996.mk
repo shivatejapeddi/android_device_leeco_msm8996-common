d1e6c980282c9d55cf198a14b9a18d747235b97f
shivatejapeddi <shivatejapeddi@gmail.com>
2018-04-04 19:37:08 +0530

    fix sensor orientation and enable sound trigger

--- msm8996.mk
+++ msm8996.mk
@@ -110,6 +110,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
     audiod \
     android.hardware.audio.effect@2.0-service \
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.0-service \
     audio.a2dp.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
