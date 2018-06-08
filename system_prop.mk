c5c5cffd96d817f953e1bd033e141f1b08fb2813
shivatejapeddi <shivatejapeddi@gmail.com>
2018-06-09 00:56:00 +0530

    import some changes from rrps

--- system_prop.mk
+++ system_prop.mk
@@ -230,3 +230,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.VT_HYBRID_ENABLE=1 \
     persist.dbg.wfc_avail_ovr=1
+
+
+# RRPS specific tweaks
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.pm.krnl_wl_block=1
