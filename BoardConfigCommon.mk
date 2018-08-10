dac812aba329a4812b383ac56ce86e45dcdd4946
Bruno Martins <bgcngm@gmail.com>
2018-08-10 23:43:04 +0100

    [DNM] msm8996-common: Pie bringup
    Change-Id: I25cc6533e7e74477d2856833f7e41a730baa7ff5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -251,7 +251,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 TARGET_RIL_VARIANT := caf
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+#include device/qcom/sepolicy/sepolicy.mk
+#BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 -include vendor/leeco/msm8996-common/BoardConfigVendor.mk
