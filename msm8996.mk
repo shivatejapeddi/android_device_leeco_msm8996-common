06e44b2134a7fef7ad4ae60cb117daa485df9044
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-09-01 09:44:56 +0200

    msm8996: lights: add commonized native binderized HAL
    Change-Id: Ib0aeefeb3dfe537bc80c7b3fef9838c64473873f

--- msm8996.mk
+++ msm8996.mk
@@ -260,10 +260,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    android.hardware.light@2.0 \
-    lights.msm8996
+    android.hardware.light@2.0-service.leeco_8996
 
 # LiveDisplay native
 #RODUCT_PACKAGES += \
