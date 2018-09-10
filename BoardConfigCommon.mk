6295993094cbee361ffe0d4ace03e14cfc97f7dc
shivatejapeddi <shivatejapeddi@gmail.com>
2018-09-11 00:16:24 +0530

    msm8996-common : bring Healthd inline with caf

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -187,6 +187,7 @@ TARGET_USES_MKE2FS := true
 
 # Power
 TARGET_HAS_NO_WIFI_STATS := true
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.msm
 
 # Recovery
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_leeco
