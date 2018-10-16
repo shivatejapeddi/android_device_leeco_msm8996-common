50943dd96f3d73493003553dd3797d7fceb4e1a6
shivatejapeddi <shivatejapeddi@gmail.com>
2018-10-16 15:37:27 +0530

    msm8996 : Squash PowerHal and Ramdisk Changes for EAS and Interactive

--- msm8996.mk
+++ msm8996.mk
@@ -147,6 +147,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.rc \
+    init.qcom.power.rc \
     init.qcom.sh \
     init.qcom.bt.sh \
     init.qcom.usb.rc \
@@ -305,12 +306,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
-    power.qcom
+    android.hardware.power@1.1-service.x2
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+PRODUCT_PROPERTY_OVERRIDES += ro.hardware.power=x2
 
 # Qualcomm
 PRODUCT_COPY_FILES += \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -29,6 +29,15 @@ LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE            := init.qcom.power.rc
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.qcom.power.rc
+LOCAL_VENDOR_MODULE     := true
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE            := init.spectrum.rc
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
