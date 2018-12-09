941c3f5e90b50e2019649d098704a421ac2545c3
shivatejapeddi <shivatejapeddi@gmail.com>
2018-12-09 17:14:54 +0000

    fi

--- extract-files.sh
+++ extract-files.sh
@@ -27,7 +27,7 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
 LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/aosp/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -57,8 +57,6 @@ fi
 
 # Initialize the helper for common
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true "$CLEAN_VENDOR"
-
-extract "$MY_DIR"/proprietary-files-qc-perf.txt "$SRC" "$SECTION"
 
 extract "$MY_DIR"/proprietary-files-qc.txt "$SRC" "$SECTION"
 
